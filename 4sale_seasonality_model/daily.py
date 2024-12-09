8836b49410eb67f9a3111564aff6529accf03057
moshebl90 <71571905+moshebl90@users.noreply.github.com>
2024-12-09 18:15:31 +0200

    Update daily.py

--- 4sale_seasonality_model/daily.py
+++ 4sale_seasonality_model/daily.py
@@ -4,9 +4,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 
-def run(final_data, selected_level_1):
+def run(selected_level_1):
     st.header("Daily Seasonality Analysis")
-    final_data = st.session_state.final_data 
+    final_data = pd.read_csv("final_data.csv")
     final_data["TIMESTAMP"] = pd.to_datetime(final_data["TIMESTAMP"])
     final_data["day"] = final_data["TIMESTAMP"].dt.day
     final_data["Level-1"] = final_data['Level-1'].str.replace('--_--', '').str.strip()
@@ -14,6 +14,7 @@ def run(final_data, selected_level_1):
 
     if selected_level_1 != "All":
         final_data = final_data[final_data["Level-1"] == selected_level_1]
+
 
     daily_data = final_data.groupby(["Level-1", "day"]).agg(
         revenue=("PRICE", "sum"),
@@ -25,12 +26,13 @@ def run(final_data, selected_level_1):
         series = daily_data[daily_data["Level-1"] == level]["revenue"].fillna(0)
 
         try:
+
             model = ARIMA(series, order=(1, 1, 1))
             model_fit = model.fit()
             forecast = model_fit.fittedvalues
             forecast.iloc[0] = series.iloc[0]
             forecast_mean = forecast.mean()
-            if forecast_mean > 1e-6:  
+            if forecast_mean > 1e-6:  # Prevent division by zero
                 daily_data.loc[
                     daily_data["Level-1"] == level, "revenue_index"
                 ] = forecast / forecast_mean
@@ -39,7 +41,7 @@ def run(final_data, selected_level_1):
                     daily_data["Level-1"] == level, "revenue_index"
                 ] = 1
 
-         except Exception as e:
+        except Exception as e:
             st.warning(f"ARIMA failed for Level-1: {level} due to {e}")
             daily_data.loc[
                 daily_data["Level-1"] == level, "revenue_index"
